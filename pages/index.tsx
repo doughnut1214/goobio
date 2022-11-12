80f23e743ef70b51a11b7f6b5675f95b866c5f34
doughnut1214 <doughnut1214@gmail.com>
2022-11-12 16:18:59 -0500

    example API fetch, added next-session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Router } from 'next/router'
-import { useEffect, useState } from 'react'
+import nextSession from 'next-session'
+
+
+
 import createAccessToken from '../auth/auth'
-const Home: NextPage = ({ data }: any) => {
+const Home: NextPage = ({ achievementPoints }: any) => {
  
 
 
@@ -17,7 +19,7 @@ const Home: NextPage = ({ data }: any) => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1>GoobIO</h1>
-        {data.access_token}
+        {achievementPoints}
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
@@ -34,10 +36,22 @@ const Home: NextPage = ({ data }: any) => {
     </div>
   )
 }
+type token = {
+  access_token: string,
+  token_type: string,
+  expires_in: number,
+  sub: string
+}
+//todo: make new dynamic page routes, /{server}/{character}, do API fetches there using this token
+//this is an example of an API response, but this thing is FAT, cannot send entire result and still remain performant
+//todo: remove any from the data token, figure out casting unkown to type token
 export async function getServerSideProps() {
-  const data = await  createAccessToken()
-
+  const data: any  = await  createAccessToken()
+  const url = `https://us.api.blizzard.com/profile/wow/character/stormrage/fenloron/achievements?namespace=profile-us&locale=en_US&access_token=${data.access_token}`
+  const results = await fetch(url)
+  const test = await results.json()
+  const achievementPoints = test.total_quantity
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { achievementPoints } }
 }
 export default Home
